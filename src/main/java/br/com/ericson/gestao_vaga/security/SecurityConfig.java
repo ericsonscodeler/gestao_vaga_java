1adf736f15496ece7bcca631fcd37f231f2c524c
Ericson Scodeler <ericson.br@hotmail.com>
2024-07-10 20:11:42 -0300

    create a secutiry config

---
+++ src/main/java/br/com/ericson/gestao_vaga/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.com.ericson.gestao_vaga.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+}
