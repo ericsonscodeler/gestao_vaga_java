1198f328a3e65b3a4c0de8f9183ce717ebb3cbc4
Ericson Scodeler <ericson.br@hotmail.com>
2024-07-12 20:18:57 -0300

    fix authcompany controller and implements SWAGGER

--- src/main/java/br/com/ericson/gestao_vaga/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/ericson/gestao_vaga/modules/company/controllers/AuthCompanyController.java
@@ -5,6 +5,7 @@ import javax.naming.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class AuthCompanyController {
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
 
-    @RequestMapping("/auth")
+    @PostMapping("/auth")
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDto authCompanyDto) {
 
         try {
