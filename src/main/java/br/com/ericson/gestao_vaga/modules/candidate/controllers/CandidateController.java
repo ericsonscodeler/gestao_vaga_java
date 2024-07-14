d59f1cebd2ebd980430baf5232bc777114d4e085
Ericson Scodeler <ericson.br@hotmail.com>
2024-07-13 23:20:12 -0300

    config swagger description for route get candidate

--- src/main/java/br/com/ericson/gestao_vaga/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/ericson/gestao_vaga/modules/candidate/controllers/CandidateController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ericson.gestao_vaga.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.ericson.gestao_vaga.modules.candidate.entities.CandidateEntity;
 import br.com.ericson.gestao_vaga.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.ericson.gestao_vaga.modules.candidate.useCases.ListAllJobsByFilterUseCase;
@@ -54,6 +55,15 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Infomações do candidato")
+    @Operation(summary = "Perfil do candidate", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "User not found")
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
 
         var idCandidate = request.getAttribute("candidate_id");
--- src/main/java/br/com/ericson/gestao_vaga/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/ericson/gestao_vaga/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.ericson.gestao_vaga.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
 
+    @Schema(example = "Desenvolvedor JavaScript")
     private String description;
+
+    @Schema(example = "teste")
     private String username;
+
+    @Schema(example = "João")
     private String name;
+
+    @Schema(example = "joao@developer.com.br")
     private String email;
     private UUID id;
 }
